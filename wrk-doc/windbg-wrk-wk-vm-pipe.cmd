69952ffb7722ddd673ddfacd743cd41616bf45c8
unknown <Administrator@songmin-vsdev.ARCSERVE.COM>
2019-10-04 13:08:49 +0800

    Signed-off-by: unknown <Administrator@songmin-vsdev.ARCSERVE.COM>

--- wrk-doc/windbg-wrk-wk-vm-pipe.cmd
+++ wrk-doc/windbg-wrk-wk-vm-pipe.cmd
@@ -1,5 +1,5 @@
-set volume_root=\\songmin-9010\e$
+set volume_root=D:
 set wrk=%~dp0
 set windbgargs=-k com:pipe,port=\\.\pipe\wrk-debug,resets=0,reconnect -srcpath %wrk%\BASE\NTOS
-set _NT_SYMBOL_PATH=%wrk%\..\base\ntos\build\exe;%volume_root%\pdb;srv*%volume_root%\pdb*http://msdl.microsoft.com/download/symbols
+set _NT_SYMBOL_PATH=%wrk%\..\base\ntos\build\exe;%CORE_ROOT%;%CORE_ROOT%\pdb;%volume_root%\pdb;srv*%volume_root%\pdb*http://msdl.microsoft.com/download/symbols
 start "" "C:\Program Files (x86)\Windows Kits\10\Debuggers\x64\windbg.exe" %windbgargs%
