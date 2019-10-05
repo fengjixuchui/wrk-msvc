fa78dd956cb6b0e9f67c039b6c3de46f4de6fb1a
smartmaster <smart.master@live.com>
2019-10-06 00:25:03 +0800

    baudrate 115200 debug settings Signed-off-by: smartmaster <smart.master@live.com>

--- wrk-doc/windbg-reactos-esx-vm-com2.cmd
+++ wrk-doc/windbg-reactos-esx-vm-com2.cmd
@@ -1,6 +1,6 @@
 set volume_root=D:
 set ros=D:\git-repo\reactos.git
-set windbgargs=-k com:port=com2 -srcpath %ros%
+set windbgargs=-k com:port=com2,baud=115200 -srcpath %ros%
 set _NT_SYMBOL_PATH=%ros%\output-vs2019-i386\msvc_pdb;%CORE_ROOT%;%CORE_ROOT%\pdb;%volume_root%\pdb;srv*%volume_root%\pdb*http://msdl.microsoft.com/download/symbols
 start "" "C:\Program Files (x86)\Windows Kits\10\Debuggers\x64\windbg.exe" %windbgargs%
 
--- wrk-doc/windbg-wrk-esx-vm.cmd
+++ wrk-doc/windbg-wrk-esx-vm.cmd
@@ -1,6 +1,6 @@
 set volume_root=D:
 set wrk=%~dp0
-set windbgargs=-k com:port=com1 -srcpath %wrk%\BASE\NTOS
+set windbgargs=-k com:port=com1,baud=115200 -srcpath %wrk%\BASE\NTOS
 set _NT_SYMBOL_PATH=%wrk%\..\base\ntos\build\exe;%CORE_ROOT%;%CORE_ROOT%\pdb;%volume_root%\pdb;srv*%volume_root%\pdb*http://msdl.microsoft.com/download/symbols
 start "" "C:\Program Files (x86)\Windows Kits\10\Debuggers\x64\windbg.exe" %windbgargs%
 
